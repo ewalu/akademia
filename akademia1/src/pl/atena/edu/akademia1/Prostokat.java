670c8c0b45430321d64298193f3afe8f349996a1
ewalu <ewa_lugin@tlen.pl>
2017-09-10 15:40:40 +0200

    zmiany if

--- akademia1/src/pl/atena/edu/akademia1/Prostokat.java
+++ akademia1/src/pl/atena/edu/akademia1/Prostokat.java
@@ -15,6 +15,24 @@ public class Prostokat {
 	 */
 	private static Scanner scanner = new Scanner(System.in);
 	
+	public static final double MIN = 1;
+	public static final double MAX = 10;
+	
+	
+	/**
+	 * @param liczba
+	 * @param komunikat
+	 * @return
+	 * metoda sprawdza, czy liczba jest z zakresu 1..10, zwraca true, lub false+komunikat
+	 */
+	public static boolean sprawdz (double liczba, String komunikat) {
+		if (!(liczba>=MIN && liczba<=MAX)) {
+			System.out.println("Liczba " + komunikat + " jest spoza zakresu [1,10]");
+			return false;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Podaj d³ugoœæ boku a:");
@@ -23,21 +41,23 @@ public class Prostokat {
 		System.out.println("Podaj d³ugoœæ boku b:");
 		String sb = scanner.nextLine();
 		
-		double a = 0;
+		double a = Double.valueOf(sa);
 		
-		try {
+		/*try {
 			a = Double.valueOf(sa);
 		} catch (Exception e) {
 			System.out.println("A nie jest liczb¹.");
-		}
+		}*/
 		
 		double b = Double.valueOf(sb);
 		
-		double pole = a*b;
+		boolean testA = sprawdz(a,"a"); //sprawdzenie i wyœwietlnie komunikatu
+		boolean testB = sprawdz(b,"b");
 		
-		//System.out.println("D³ugoœæ boku a = "+a);
-		//System.out.println("D³ugoœæ boku b = "+b);
-		System.out.println("Pole = "+pole);
+		if (testA && testB) {
+			double pole = a*b;
+			System.out.println("Pole = "+pole);
+		}
 
 	}
 
