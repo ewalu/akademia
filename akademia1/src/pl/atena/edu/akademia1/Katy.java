385d2a1b8f136b7f4d85126d50f1a8a10aa302e7
ewalu <ewa_lugin@tlen.pl>
2017-09-17 14:32:55 +0200

    katy 2

--- akademia1/src/pl/atena/edu/akademia1/Katy.java
+++ akademia1/src/pl/atena/edu/akademia1/Katy.java
@@ -1,9 +1,30 @@
 package pl.atena.edu.akademia1;
 
+import java.util.Scanner;
+
 public class Katy {
+	
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public static boolean normalize (int liczba) {
+		int numb1 = Math.floorMod(liczba, 360);
+		System.out.println("Normalizacja: "+ numb1);
+		return true;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Podaj liczbê ca³kowit¹:");
+		String smsc = scanner.nextLine();
+		int numb;
+		
+		try {
+			numb = Integer.valueOf(smsc);
+		} catch (Exception e) {
+			System.out.println("To nie jest liczba ca³kowita.");
+			return;
+		}
+		
+		boolean wynik = normalize(numb);
 
 	}
 
