c76a166eee25562692b672f1f9c3826adc38977a
ewalu <ewa_lugin@tlen.pl>
2017-09-24 09:52:25 +0200

    nr 3

--- akademia1/src/pl/atena/edu/akademia1/Lekcja2.java
+++ akademia1/src/pl/atena/edu/akademia1/Lekcja2.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Lekcja2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String tekst = "aaaaaaa \n 79797hhhhhhhhhh";
 		Scanner scanner = new Scanner(tekst);
 		System.out.println("aaaa " + scanner.hasNext());
 		System.out.println("nnn " + scanner.nextLine());
+		System.out.println("nnn " + scanner.nextLine());
+
+		System.out.println(1000.0/3.0);
+		System.out.printf("%8.2f\n",1000.0/3.0);
+		System.out.printf("%.2f\n",1000.0/3.0);
+		System.out.printf("%+.4f\n",1000.0/3.0);
+		
+		String ss = String.format("%s %d","eeee",444);
+		System.out.println(ss);
+		
+		scanner.close();
 		
 
 	}
---
+++ akademia1/src/pl/atena/edu/akademia1/Pi.java
@@ -0,0 +1,10 @@
+package pl.atena.edu.akademia1;
+
+public class Pi {
+
+	public static void main(String[] args) {
+		System.out.printf("%.5f",Math.PI);
+
+	}
+
+}
