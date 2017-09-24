71956f84c40d39e570e408abef88e3d4b9aa0826
ewalu <ewa_lugin@tlen.pl>
2017-09-24 09:27:49 +0200

    20170924 nr 1

---
+++ akademia1/src/pl/atena/edu/akademia1/Lekcja2.java
@@ -0,0 +1,17 @@
+package pl.atena.edu.akademia1;
+
+import java.util.Scanner;
+
+public class Lekcja2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String tekst = "aaaaaaa \n 79797hhhhhhhhhh";
+		Scanner scanner = new Scanner(tekst);
+		System.out.println("aaaa " + scanner.hasNext());
+		System.out.println("nnn " + scanner.nextLine());
+		
+
+	}
+
+}
