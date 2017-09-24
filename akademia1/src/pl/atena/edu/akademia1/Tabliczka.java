8d270b029990618bcaa6b45e647cbc0b9e74fd21
ewalu <ewa_lugin@tlen.pl>
2017-09-24 11:47:40 +0200

    tabliczka 2

--- akademia1/src/pl/atena/edu/akademia1/Tabliczka.java
+++ akademia1/src/pl/atena/edu/akademia1/Tabliczka.java
@@ -12,9 +12,22 @@ public class Tabliczka {
 			for (int j = 1; j<=10; j++) {
 				System.out.printf("%4d", i*j);
 			}
-			System.out.println("");
+			System.out.println();
 		}
+		
+		for (int i = 0; i<=10; i++) {
+			for (int j = 0; j<=10; j++) {
+				if (i==0) {
+				System.out.printf("%4d", j);
+			} else if (j==0) {
+				System.out.printf("%4d", i);
+			} else {
+				System.out.printf("%4d", i*j);
+			}
+		}
+			System.out.println();
 
+	}
 	}
 
 }
