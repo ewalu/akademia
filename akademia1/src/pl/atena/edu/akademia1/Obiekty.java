cd64476dcc0081cb33900b42603fc1505b5f0b6a
ewalu <ewa_lugin@tlen.pl>
2017-09-24 16:56:35 +0200

    obiekty

--- akademia1/src/pl/atena/edu/akademia1/Obiekty.java
+++ akademia1/src/pl/atena/edu/akademia1/Obiekty.java
@@ -4,7 +4,20 @@ public class Obiekty {
 
 	public static void main(String[] args) {
 		Osoba osoba1 = new Osoba();
-		Osoba osoba2 = new Osoba("aaa");
+		Osoba osoba2 = new Osoba(23);
+		Osoba osoba3 = new Osoba(23,180);
+		Osoba osoba4 = new Osoba("Ewa","£ugin", 12, 60);
+		Osoba osoba5 = Osoba.instance("Jan");
+		
+		osoba5.dodajLat(3);
+		
+		System.out.println(osoba1);
+		System.out.println(osoba2);
+		System.out.println(osoba3);
+		System.out.println(osoba4);
+		System.out.println(osoba5);
+		
+		System.out.println(osoba2.pelnoletni());
 
 	}
 
--- akademia1/src/pl/atena/edu/akademia1/Osoba.java
+++ akademia1/src/pl/atena/edu/akademia1/Osoba.java
@@ -1,14 +1,55 @@
 package pl.atena.edu.akademia1;
 
 public class Osoba {
+	public static final int PELNOLETNOSC = 18;
+	private String imie;
+	private String nazwisko;
+	private int wiek;
+	private int wzrost;
 	
 	//konstruktory
 	public Osoba() {
 		System.out.println("Jestem");
 	}
 	
-	public Osoba(String komunikat) {
-		System.out.println(komunikat);
+	public Osoba(int wiek) {
+		this.wiek = wiek;
 	}
+	
+	public Osoba(int wiek, int wzrost) {
+		this(wiek); //wywo³uje powy¿szy konstruktor
+		this.wzrost = wzrost;
+	}
+	
+	public Osoba(String imie, String nazwisko, int wiek, int wzrost) {
+		this(wiek, wzrost);
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+	}
+	
+	private Osoba(String imie) {
+		this.imie = imie;
+	}
+	
+	public static Osoba instance(String imie) {
+		return new Osoba(imie);
+	}
+	
+	public void dodajLat(int lata) {
+		this.wiek += lata;
+	}
+	
+	public boolean pelnoletni() {
+		return this.wiek < PELNOLETNOSC ? false: true;
+	}
+
+	@Override //adnotacja nadpisanie
+	public String toString() {
+		return String.format("Osoba [imie=%s, nazwisko=%s, wiek=%s, wzrost=%s]", imie, nazwisko, wiek, wzrost);
+	}
+
+
+	
+	
 
 }
