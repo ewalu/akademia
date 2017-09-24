bd8ca8cd5f02517f5e3516b579151839392c14b1
ewalu <ewa_lugin@tlen.pl>
2017-09-24 15:48:31 +0200

    obiekty 1

---
+++ akademia1/src/pl/atena/edu/akademia1/Obiekty.java
@@ -0,0 +1,11 @@
+package pl.atena.edu.akademia1;
+
+public class Obiekty {
+
+	public static void main(String[] args) {
+		Osoba osoba1 = new Osoba();
+		Osoba osoba2 = new Osoba("aaa");
+
+	}
+
+}
---
+++ akademia1/src/pl/atena/edu/akademia1/Osoba.java
@@ -0,0 +1,14 @@
+package pl.atena.edu.akademia1;
+
+public class Osoba {
+	
+	//konstruktory
+	public Osoba() {
+		System.out.println("Jestem");
+	}
+	
+	public Osoba(String komunikat) {
+		System.out.println(komunikat);
+	}
+
+}
