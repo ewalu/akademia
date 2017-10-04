6eaf443aa8d3b650c51ee50906dd839f93edfbd9
ewalu <ewa_lugin@tlen.pl>
2017-10-04 21:18:02 +0200

    kwadrat 2

--- akademia1/src/pl/atena/edu/akademia1/MagicSquare.java
+++ akademia1/src/pl/atena/edu/akademia1/MagicSquare.java
@@ -2,24 +2,35 @@ package pl.atena.edu.akademia1;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class MagicSquare {
+	
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int [][] kwadrat = new int[3][3];
+		System.out.println("Podaj wymiar kwadratu");
+		int wymiar = scanner.nextInt();
+		int [][] kwadrat = new int[wymiar][wymiar];
 		int [] sumy = new int [kwadrat.length*2+2];
-		Random generator = new Random();
 		
 		for(int i=0; i<sumy.length; i++) {
 			sumy[i] = 0;
 			
 		}
+		System.out.println("Podaj kwadrat");
+		for (int i=0; i<kwadrat.length; i++) {
+			for (int j=0; j<kwadrat.length; j++) {
+			kwadrat[i][j] = scanner.nextInt();
+			
+			}
+		}
 		
 		for (int i=0; i<kwadrat.length; i++) {
 			for (int j=0; j<kwadrat.length; j++) {
 			//kwadrat[i][j] = generator.nextInt(9);
-			kwadrat[i][j] = 2;
-			System.out.print(kwadrat[i][j]);
+			//kwadrat[i][j] = 2;
+			System.out.printf("%4d",kwadrat[i][j]);
 			}
 			System.out.println();
 			
@@ -37,6 +48,8 @@ public class MagicSquare {
 		int suma = 0;
 		for (int i = 0; i<sumy.length; i++)	{
 			suma+=sumy[i];
+			System.out.print(sumy[i]+" ");
+			
 		}
 		
 		if (suma/sumy.length == sumy[0]) {
